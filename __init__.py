783500ae1977c58ce4335138f235149600b81392
CalvoM <mwadimemakokha@ieee.org>
2019-11-20 02:00:17 +0300

    Integration of the modules

--- __init__.py
+++ __init__.py
@@ -3,3 +3,8 @@ from flask import Flask
 post_app = Flask(__name__)
 
 from .models import Post,db
+from .apis import post_api
+post_app.register_blueprint(post_api)
+@post_app.route('/')
+def home():
+    return "Am home"
---
+++ app.py
@@ -0,0 +1,4 @@
+from post_app_backend import post_app
+
+if __name__ == "__main__":
+    post_app.run()
