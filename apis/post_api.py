a18c14bfcc939f0915bd1c2883561ffdbbf079a1
CalvoM <mwadimemakokha@ieee.org>
2019-11-20 01:50:36 +0300

    Add comment

--- apis/post_api.py
+++ apis/post_api.py
@@ -4,6 +4,7 @@ post_api = Blueprint('post_api',__name__)
 
 @post_api.route('/get/posts')
 def get_all_posts():
+    '''Get all posts'''
     posts = Post.query.all()
     posts_list = [p.deserialise for p in posts]
     return jsonify(posts=posts_list)
